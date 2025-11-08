cbf50db9401e16d184e9915f74fffb706ad1d618
istiakEm0n <istiakemon231@gmail.com>
2025-11-08 22:44:27 +0600

    Creates .env file and installs dotenv for port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import express, { type Express, type Request, type Response } from 'express';
 const app: Express = express();
-const port: number = 5000;
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT: number = Number(process.env.PORT) || 5000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World from TypeScript and Express!');
@@ -10,6 +13,6 @@ app.get('/health', (req: Request, res: Response) => {
   res.send('OK!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
