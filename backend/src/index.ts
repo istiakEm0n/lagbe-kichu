ca9614610ef3f4bc61b83137f506007551c2c74c
istiakEm0n <151034467+istiakEm0n@users.noreply.github.com>
2025-11-08 22:31:17 +0600

    changes port to 5000 and an health status endpoint (#7)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
 import express, { type Express, type Request, type Response } from 'express';
 const app: Express = express();
-const port: number = 3000;
+const port: number = 5000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World from TypeScript and Express!');
+});
+
+app.get('/health', (req: Request, res: Response) => {
+  res.send('OK!');
 });
 
 app.listen(port, () => {
